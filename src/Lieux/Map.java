68a18a36b1a9c45bf7a98b1ab45dd7ced8936d2a
Maetis <41517613+Maetis0511@users.noreply.github.com>
2022-05-08 01:00:26 +0200

    ajout de méthodes à la classe Map

--- src/Lieux/Map.java
+++ src/Lieux/Map.java
@@ -1,6 +1,23 @@
 package Lieux;
 
+import java.util.Set;
+
 public class Map {
     private Graphe<Salle> map;
 
+    public Map() {
+        this.map = new Graphe<Salle>();
+    }
+
+    public void addSalle(Salle s) {
+        this.map.ajouterSommet(s);
+    }
+
+    public void addLien(Salle s1, Salle s2) {
+        this.map.ajouterArete(s1, s2, 1);
+    }
+
+    public Set<Salle> sallesAdjacentes(Salle s) {
+        return this.map.getVoisins(s);
+    }
 }
--- src/Personnages/Hostile.java
+++ src/Personnages/Hostile.java
@@ -10,13 +10,14 @@ public class Hostile extends Personnages {
     private int nbLoot;
     private float chanceLoot;
 
-    public Hostile(String nom, int vie,int attaque,Item loot , int nbLoot,int niveau,int xp, int chanceLoot) {
+    public Hostile(String nom, int vie, int attaque, Item loot, int nbLoot, int niveau, int xp, int chanceLoot) {
         super(nom, vie, attaque, niveau, xp);
         this.loot = loot;
         this.nbLoot = nbLoot;
         this.chanceLoot = chanceLoot;
 
     }
+
     public void attaque(Joueur joueur){
         Random r = new Random();
         int nb = r.nextInt(1000);
@@ -27,8 +28,6 @@ public class Hostile extends Personnages {
         else{
             joueur.perteVie(this.getAttaque());
             System.out.println("Vous avez perdu " + this.getAttaque() + " points de vie de la part de " + this.getNom());
-
         }
     }
-
 }
